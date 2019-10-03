731d0841d78c7f1cc66b6ca0a985cfecb5ec6d0f
oloniyo victor <oloniyov@gmail.com>
2019-10-03 12:00:35 +0100

    change IP to Board

--- myApp/settings.py
+++ myApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7&4()ra5^dzbp6mn$2f4fq@to!78&w5_f&ish^%3j!h60ru13i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.100.87']
+ALLOWED_HOSTS = ['192.168.100.88']
 
 
 # Application definition
